42b9ba8bb9b5e8f0076ad8f5379f202986c7782f
Geno Genov <g.genov1993@gmail.com>
2014-11-05 20:20:39 +0200

    project skeleton

---
+++ HelpMeGraduate/HelpMeGraduate.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HelpMeGraduate.Web.Startup))]
+namespace HelpMeGraduate.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
