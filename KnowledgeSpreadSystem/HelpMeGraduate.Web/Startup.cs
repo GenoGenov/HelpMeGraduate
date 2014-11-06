ccee4d04a16dfe69079a0296eac59ffdd4843e77
Geno Genov <g.genov1993@gmail.com>
2014-11-06 23:00:48 +0200

    (almost) data layer

---
+++ KnowledgeSpreadSystem/HelpMeGraduate.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using KnowledgeSpreadSystem.Web;
+
+using Microsoft.Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace KnowledgeSpreadSystem.Web
+{
+    using Owin;
+
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            this.ConfigureAuth(app);
+        }
+    }
+}
